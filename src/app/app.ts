09b35a09bf85c10897b88980df9c7e99336fd54b
adela miclea <adelamiclea709@yahoo.com>
2026-05-24 17:29:19 +0300

    PART1

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { Hero } from './components/hero/hero';
 import { Story } from './components/story/story';
 import { EventDetails } from './components/event-details/event-details';
 import { Timeline } from './components/timeline/timeline';
-import { Countdown } from './components/countdown/countdown';
+import {  CountdownComponent } from './components/countdown/countdown';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +14,13 @@ import { Countdown } from './components/countdown/countdown';
     Story,
     EventDetails,
     Timeline,
-    Countdown
-  ],
+    CountdownComponent,
+    CountdownComponent
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App {
-
+  weddingDate = new Date('2026-08-15T00:00:00');
 }
